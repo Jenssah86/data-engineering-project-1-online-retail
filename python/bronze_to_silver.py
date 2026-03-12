fa507d8eeed513b63ac88650166990d96720540f
Jenssah86 <tim.janssen@hotmail.nl>
2026-03-12 15:03:21 +0100

    v1.0

--- python/bronze_to_silver.py
+++ python/bronze_to_silver.py
@@ -9,112 +9,105 @@ from dotenv import load_dotenv
 # ---------------------------------------------------------------------------------------
 # 1. Load environment variables
 # ---------------------------------------------------------------------------------------
-load_dotenv()  # leest .env in
+load_dotenv()
 
-# haalt de database connectie gegevens op uit de omgevingsvariabelen, deze moeten in een .env bestand staan in dezelfde map als dit script
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_HOST = os.getenv("MYSQL_HOST")
 MYSQL_DB = os.getenv("MYSQL_DB")
 
+# ---------------------------------------------------------------------------------------
+# 2. Absolute project directory (BELANGRIJK VOOR TAAKPLANNER)
+# ---------------------------------------------------------------------------------------
+BASE_DIR = Path(r"D:\Studie\GitHub portfolio\data-engineering-project-1-online-retail")
+
+BRONZE_DIR = BASE_DIR / "bronze"
+PROCESSED_DIR = BRONZE_DIR / "verwerkt"
+
+# Zorg dat de mappen bestaan
+BRONZE_DIR.mkdir(exist_ok=True)
+PROCESSED_DIR.mkdir(exist_ok=True)
 
 # ---------------------------------------------------------------------------------------
-# 2. Instellingen
-# ---------------------------------------------------------------------------------------
-BRONZE_DIR = Path("bronze")                     # map waar de CSV-bestanden worden geplaatst
-PROCESSED_DIR = BRONZE_DIR / "verwerkt"         # map waar de verwerkte bestanden naartoe worden verplaatst
-PROCESSED_DIR.mkdir(exist_ok=True)              # zorgt ervoor dat de map 'verwerkt' bestaat, anders wordt deze gemaakt
-
-
-# ---------------------------------------------------------------------------------------
-# 3. Bronze CSV-bestanden zoeken # functie die CSV-files in de bronze map zoekt en een lijst teruggeeft voor in de FOR loop van de main_flow functie
+# 3. Bronze CSV-bestanden zoeken
 # ---------------------------------------------------------------------------------------
 def get_bronze_files():
     return list(BRONZE_DIR.glob("online_retail_*.csv"))
 
-
 # ---------------------------------------------------------------------------------------
-# 4. Data inlezen # functie die een CSV-bestand inleest en teruggeeft als DataFrame
+# 4. Data inlezen
 # ---------------------------------------------------------------------------------------
-def load_bronze_file(path: Path) -> pd.DataFrame:   # vraagt om een pad naar een CSV-bestand en geeft een DataFrame terug
+def load_bronze_file(path: Path) -> pd.DataFrame:
     print(f"Inlezen: {path.name}")
-    return pd.read_csv(path, sep=";")               # leest het CSV-bestand in met een puntkomma als scheidingsteken, en geeft DataFrame terug
-
+    return pd.read_csv(path, sep=";")
 
 # ---------------------------------------------------------------------------------------
-# 5. Data opschonen # functie die de data opschoont en teruggeeft als DataFrame
+# 5. Data opschonen
 # ---------------------------------------------------------------------------------------
-def clean_data(df: pd.DataFrame) -> pd.DataFrame:                                      # vraagt om een df en geeft een geschoonde df terug
-    df.columns = df.columns.str.strip().str.replace(" ", "")                           # verwijdert eventuele spaties in de kolomnamen
-    df = df.dropna(subset=["CustomerID", "Invoice", "StockCode"])                      # verwijdert rijen waar deze belangrijke kolommen leeg zijn
-    df["InvoiceDate"] = pd.to_datetime(df["InvoiceDate"],                              # kolom om zetten naar datetime, en haalt alleen de datum eruit
-    errors="coerce",
-    dayfirst=True
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    df.columns = df.columns.str.strip().str.replace(" ", "")
+    df = df.dropna(subset=["CustomerID", "Invoice", "StockCode"])
+    df["InvoiceDate"] = pd.to_datetime(
+        df["InvoiceDate"],
+        errors="coerce",
+        dayfirst=True
     ).dt.date
-    df["Quantity"] = pd.to_numeric(df["Quantity"], errors="coerce")                    # probeert de 'Quantity' kolom om te zetten naar numeriek
-    df["Price"] = df["Price"].str.replace(",", ".", regex=False)                       # vervangt komma's door punten in de 'Price' kolom
-    df["Price"] = pd.to_numeric(df["Price"], errors="coerce")                          # probeert de 'UnitPrice' kolom om te zetten naar numeriek
-    df["Total_price"] = (df["Quantity"] * df["Price"]).round(2)                        # voegt een nieuwe kolom 'total_amount' berekening toe
-
+    df["Quantity"] = pd.to_numeric(df["Quantity"], errors="coerce")
+    df["Price"] = df["Price"].str.replace(",", ".", regex=False)
+    df["Price"] = pd.to_numeric(df["Price"], errors="coerce")
+    df["Total_price"] = (df["Quantity"] * df["Price"]).round(2)
     return df
 
-
 # ---------------------------------------------------------------------------------------
-# 6. MySQL connectie # functie die een SQLAlchemy engine maakt voor de MySQL database
+# 6. MySQL connectie
 # ---------------------------------------------------------------------------------------
 def get_engine():
-    url = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"   # bouwt de database URL op basis van de omgevingsvariabelen
-    return create_engine(url)   # maakt een SQLAlchemy engine aan met de opgegeven URL, voor verbinding met MySQL database
-
+    url = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+    return create_engine(url)
 
 # ---------------------------------------------------------------------------------------
-# 7. Wegschrijven naar Silver # functie die de opgeschoonde data wegschrijft naar de MySQL database
+# 7. Wegschrijven naar Silver
 # ---------------------------------------------------------------------------------------
 def write_to_silver(df: pd.DataFrame):
-    engine = get_engine()          # haalt de SQLAlchemy engine op met de get_engine functie
-
-    df.to_sql(                     # gebruikt de to_sql methode van de DataFrame om de data weg te schrijven naar de database
-        name="silver_sales",       # geeft de naam van de tabel in de database waar de data naartoe wordt geschreven, in dit geval 'silver_sales'
-        con=engine,                # geeft de SQLAlchemy engine door als verbinding voor het wegschrijven van de data naar de database
-        if_exists="append",        # zorgt ervoor dat de data wordt toegevoegd aan de bestaande tabel 'silver_sales' ipv deze te overschrijven
-        index=False                # zorgt ervoor dat de DataFrame index niet wordt weggeschreven als een aparte kolom in de database
+    engine = get_engine()
+    df.to_sql(
+        name="silver_sales",
+        con=engine,
+        if_exists="append",
+        index=False
     )
-
     print("Data toegevoegd aan silver_sales.")
 
-
 # ---------------------------------------------------------------------------------------
-# 8. Verwerkt bestand verplaatsen # functie die het verwerkte bestand verplaatst naar de 'verwerkt' map
+# 8. Verwerkt bestand verplaatsen
 # ---------------------------------------------------------------------------------------
 def move_to_processed(path: Path):
-    destination = PROCESSED_DIR / path.name     # bepaalt de bestemming van het bestand in de 'verwerkt' map, met dezelfde bestandsnaam
-    shutil.move(str(path), destination)         # verplaatst het bestand van de bronze map naar de 'verwerkt' map
+    destination = PROCESSED_DIR / path.name
+    shutil.move(str(path), destination)
     print(f"Verplaatst naar: {destination}")
 
-
 # ---------------------------------------------------------------------------------------
-# 9. Main flow # hoofdfunctie dat alle bovenstaande stappen uitvoert
+# 9. Main flow
 # ---------------------------------------------------------------------------------------
 def main_flow():
     print("Zoeken naar nieuwe bronze CSV-bestanden...")
 
-    files = get_bronze_files()              # haalt alle CSV-bestanden op uit de bronze map met def function get_bronze_files()
+    files = get_bronze_files()
 
     if not files:
         print("Geen nieuwe bestanden gevonden.")
         return
 
     for file in files:
-        df_raw = load_bronze_file(file)     # leest het CSV-bestand in als een DataFrame
-        df_clean = clean_data(df_raw)       # maakt de data schoon en voegt de 'total_amount' kolom toe
-        write_to_silver(df_clean)           # schrijft de opgeschoonde data weg naar de MySQL database
-        move_to_processed(file)             # verplaatst het verwerkte bestand naar de 'verwerkt' map
+        df_raw = load_bronze_file(file)
+        df_clean = clean_data(df_raw)
+        write_to_silver(df_clean)
+        move_to_processed(file)
 
     print("Alle nieuwe bestanden zijn verwerkt en verplaatst.")
 
-
 # ---------------------------------------------------------------------------------------
-# 10. Uitvoeren # zorgt ervoor dat de main_flow alleen wordt uitgevoerd als dit script direct wordt uitgevoerd, dus niet bij een import als module
+# 10. Uitvoeren
 # ---------------------------------------------------------------------------------------
 if __name__ == "__main__":
     main_flow()
